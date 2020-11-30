790c63b611ba6e127ea14f262b40cac3298f6e28
oscarlic <lc694802624@gmail.com>
2020-11-30 10:54:47 +0800

    首页数据及轮播展示

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,55 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <!-- 导航栏 -->
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+
+    <!-- 图片轮播 -->
+    <!-- 代码抽取至HomeSwiper -->
+    <!-- <swiper>
+      <swiper-item v-for="(item, index) in banners" :key="index">
+        <a :href="item.link">
+          <img :src="item.image" alt="" />
+        </a>
+      </swiper-item>
+    </swiper> -->
+    <home-swiper :banners="banners"></home-swiper>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: "Home"
-  }
+import NavBar from "components/common/navbar/NavBar";
+import HomeSwiper from "./childComps/HomeSwiper";
+import { getHomeMultidata } from "network/home";
+
+export default {
+  name: "Home",
+  components: {
+    NavBar,
+    HomeSwiper,
+  },
+  data() {
+    return {
+      // result存储了res指向的数据，否则数据将被回收
+      // result: null
+      banners: [],
+      recommends: [],
+    };
+  },
+  created() {
+    // 1.请求多个数据
+    getHomeMultidata().then((res) => {
+      // console.log(res);
+      // this.result = res;
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    });
+  },
+};
 </script>
 
 <style scoped>
-
+.home-nav {
+  background-color: var(--color-tint);
+  color: white;
+}
 </style>
---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,35 @@
+<template>
+  <swiper>
+    <!--  新版Vue中的v-for必须有key属性！ -->
+    <swiper-item v-for="(item, index) in banners" :key="index">
+      <a :href="item.link">
+        <img :src="item.image" alt="" />
+      </a>
+    </swiper-item>
+  </swiper>
+</template>
+
+<script>
+import Swiper from "components/common/swiper/Swiper";
+import SwiperItem from "components/common/swiper/SwiperItem";
+
+export default {
+  name: "HomeSwiper",
+  // 父传子props
+  props: {
+    banners: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  components: {
+    Swiper,
+    SwiperItem,
+  },
+};
+</script>
+
+<style>
+</style>
