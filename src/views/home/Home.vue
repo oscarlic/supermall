b758e765b35db51af27560e308565a128f8c09a2
oscarlic <lc694802624@gmail.com>
2020-11-30 18:06:22 +0800

    首页商品数据的请求、保存和展示

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -19,6 +19,7 @@
       class="tab-control"
       :titles="['流行', '新款', '精选']"
     ></tab-control>
+    <goods-list :goods="goods['pop'].list"></goods-list>
 
     <ul>
       <li>列表1</li>
@@ -132,8 +133,9 @@ import FeatureView from "./childComps/FeatureView.vue";
 
 import NavBar from "components/common/navbar/NavBar";
 import TabControl from "components/content/tabControl/TabControl";
+import GoodsList from "components/content/goods/GoodsList";
 
-import { getHomeMultidata } from "network/home";
+import { getHomeMultidata, getHomeGoods } from "network/home";
 
 export default {
   name: "Home",
@@ -143,6 +145,7 @@ export default {
     FeatureView,
     NavBar,
     TabControl,
+    GoodsList,
   },
   data() {
     return {
@@ -150,16 +153,40 @@ export default {
       // result: null
       banners: [],
       recommends: [],
+      // goods的数据模型
+      goods: {
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sell: { page: 0, list: [] },
+      },
     };
   },
   created() {
-    // 1.请求多个数据
-    getHomeMultidata().then((res) => {
-      // console.log(res);
-      // this.result = res;
-      this.banners = res.data.banner.list;
-      this.recommends = res.data.recommend.list;
-    });
+    // 1.请求多个数据。两个名称一样，所有必须加上this
+    this.getHomeMultidata();
+    // 2.请求商品数据
+    this.getHomeGoods("pop");
+    this.getHomeGoods("new");
+    this.getHomeGoods("sell");
+  },
+  methods: {
+    getHomeMultidata() {
+      getHomeMultidata().then((res) => {
+        // console.log(res);
+        // this.result = res;
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      });
+    },
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1;
+      getHomeGoods(type, page).then((res) => {
+        // console.log(res);
+        // 保存首页数据。把res的数据保存到lists，好好斟酌这个语法
+        this.goods[type].list.push(...res.data.list);
+        this.goods[type].page += 1;
+      });
+    },
   },
 };
 </script>
@@ -181,5 +208,6 @@ export default {
 .tab-control {
   position: sticky;
   top: 44px;
+  z-index: 9;
 }
 </style>
