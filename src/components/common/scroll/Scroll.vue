1a67ca3000be3659b1931bdde8d6fbcc4ced7d51
oscarlic <lc694802624@gmail.com>
2020-12-02 19:08:06 +0800

    Better-Scroll引入home中

---
+++ src/components/common/scroll/Scroll.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="wrapper" ref="wrapper">
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import BScroll from "better-scroll";
+
+export default {
+  name: "Scroll",
+  data() {
+    return {
+      scroll: null,
+    };
+  },
+  mounted() {
+    // 如果使用wrapper，有可能和其他组件的自带同名wrapper冲突
+    // this.scroll = new BScroll(document.querySelector('.wrapper'), {});
+    // click默认是false，导致无法点击
+    this.scroll = new BScroll(this.$refs.wrapper, { click: true });
+  },
+};
+</script>
+
+<style scoped>
+</style>
